24383cbd8224fc3ea862f19d301354e6b4f89119
bestia.dev <info@bestia.dev>
2025-11-08 09:37:53 +0000

    update docs

--- docs/trait.impl/serde/ser/trait.Serialize.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["github_readme_copy",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"github_readme_copy/encrypt_decrypt_with_ssh_key_mod/encrypt_decrypt_mod/struct.EncryptedTextWithMetadata.html\" title=\"struct github_readme_copy::encrypt_decrypt_with_ssh_key_mod::encrypt_decrypt_mod::EncryptedTextWithMetadata\">EncryptedTextWithMetadata</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"github_readme_copy/encrypt_decrypt_with_ssh_key_mod/github_api_token_with_oauth2_mod/struct.GithubApiConfig.html\" title=\"struct github_readme_copy::encrypt_decrypt_with_ssh_key_mod::github_api_token_with_oauth2_mod::GithubApiConfig\">GithubApiConfig</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"github_readme_copy/encrypt_decrypt_with_ssh_key_mod/github_api_token_with_oauth2_mod/struct.SecretResponseAccessToken.html\" title=\"struct github_readme_copy::encrypt_decrypt_with_ssh_key_mod::github_api_token_with_oauth2_mod::SecretResponseAccessToken\">SecretResponseAccessToken</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[1369]}
---
+++ docs/trait.impl/serde_core/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["github_readme_copy",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"github_readme_copy/encrypt_decrypt_with_ssh_key_mod/encrypt_decrypt_mod/struct.EncryptedTextWithMetadata.html\" title=\"struct github_readme_copy::encrypt_decrypt_with_ssh_key_mod::encrypt_decrypt_mod::EncryptedTextWithMetadata\">EncryptedTextWithMetadata</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"github_readme_copy/encrypt_decrypt_with_ssh_key_mod/github_api_token_with_oauth2_mod/struct.GithubApiConfig.html\" title=\"struct github_readme_copy::encrypt_decrypt_with_ssh_key_mod::github_api_token_with_oauth2_mod::GithubApiConfig\">GithubApiConfig</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"github_readme_copy/encrypt_decrypt_with_ssh_key_mod/github_api_token_with_oauth2_mod/struct.SecretResponseAccessToken.html\" title=\"struct github_readme_copy::encrypt_decrypt_with_ssh_key_mod::github_api_token_with_oauth2_mod::SecretResponseAccessToken\">SecretResponseAccessToken</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1414]}
